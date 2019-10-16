3f9373bf6fbaac4ec83bd4e7beba160e5f63eb42
Nattee Niparnan <nattee@gmail.com>
2019-10-16 11:04:14 +0700

    add some solution

--- 10-16_pq_by_vector/pq_vector_fast_pop.cpp
+++ 10-16_pq_by_vector/pq_vector_fast_pop.cpp
@@ -20,6 +20,36 @@ class ArrayPQ {
 
     void push(const T& value) {
       //write your code here
+
+      //x.push_back(value);
+      //sort(x.begin(),x.end()); //  <---- this is O(n lg n)
+
+//      for (int i = 0;i < size()-1;i++) {
+//        if ((value > x[i]) && (value < x[i+1])) {
+//          x.insert(x.begin() + i + 1,value);
+//
+//        }
+
+//      bool added = false;
+//      for (int i = 0;i < size();i++) {
+//        if (value <= x[i]) {
+//          x.insert(x.begin()+i,value);
+//          added = true;
+//          break;
+//        }
+//      }
+//      if (added == false) {
+//        x.push_back(value);
+//
+//      }
+      auto it = x.begin();
+      while (it != x.end()); {
+        if (value < *it) {
+          break;
+        }
+        it++;
+      }
+      x.insert(it,value);
     }
 };
 
